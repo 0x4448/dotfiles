3b4e6efd87a32864251776f21baf74421e44bf7a
0x4448 <86135470+0x4448@users.noreply.github.com>
2022-07-24 19:29:11 +0000

    Create global gitconfig file if missing

--- bootstrap/linux.sh
+++ bootstrap/linux.sh
@@ -4,6 +4,12 @@ cd "$HOME" || exit
 echo ".dotfiles" > "$HOME/.gitignore"
 git clone --bare https://github.com/0x4448/dotfiles .dotfiles
 
+# Create global gitconfig file
+# This prevents unintentional additions to the secondary user gitconfig
+if [[ ! -f "$HOME/.gitconfig" ]]; then
+  touch "$HOME/.gitconfig"
+fi
+
 alias dotfiles='GIT_DIR="$HOME/.dotfiles/" GIT_WORK_TREE="$HOME/"'
 
 dotfiles git checkout --force
